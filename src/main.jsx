30097bc4d211bdcfd95e4a81f1fe3da60d177737
Minu Kim <minu.kim2003@gmail.com>
2022-06-29 04:51:02 +0200

    commit before styling changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalStyle } from "./GlobalStyles.jsx";
 import { PageNotFound } from "./pages/PageNotFound";
+import { Menu } from "./pages/Menu";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/menu" element={<Menu />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
